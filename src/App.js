8c25504829ca4d77cc00546c5112518a215042cf
Chloe <chloe84@msn.com>
2022-03-21 14:40:44 -0400

    added more info button and react player

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieList from './Components/MovieList/MovieList';
 import SearchBar from './Components/SearchBar/SearchBar';
 import { useEffect, useState } from "react"
 import { fetchMoviesFromDatabase, GenreContext } from './NetworkConnections'
-
+import ReactPlayer from 'react-player';
 
 
 function App() {
@@ -24,7 +24,8 @@ function App() {
       <div className="App">
         <h1>Fabulous Flix</h1>
         <SearchBar title='search'  />
-        <SingleMovie id='13'/>
+        <ReactPlayer url ='https://www.youtube.com/watch?v=ahy17q1vEWw'/>
+        <SingleMovie id='14736'/>
         <MovieList list='trending/movie/week'/>
 
 
--- src/Components/SingleMoviePage/SingleMoviePage.js
+++ src/Components/SingleMoviePage/SingleMoviePage.js
@@ -1,33 +1,23 @@
 import { useEffect, useState, useContext } from "react"
 import { fetchMoviesFromDatabase, GenreContext } from "../../NetworkConnections"
-import {Link} from "react-router-dom"
+import SingleMovie from "../SingleMovie/SingleMovie"
 
-export default function SingleMovie(props){
+
+export default function SingleMoviePage(props){
     const [movie, setMovie] = useState(props.movie)
-    const [movies, setMovies] = useState([])
-    const [searchName, setSearchName] = useState('')
     const genres = useContext(GenreContext)[movie?.original_title ? 1 : 0]
 
-    console.log(genres)
-    
     useEffect(() =>{
         if(movie) return
         fetchMoviesFromDatabase(`movie/${props?.id}`)
             .then(res => setMovie(res))
         
     },[])
-    console.log(movie?.genres_ids)
-    // Grab the genre name based on genre id
     
-    // find a genre with an id same as movie.genre_id
     return <div className='single-movie'>
         <h3>{movie?.original_title || movie?.original_name}</h3>
         {movie && <img alt={`poster for ${movie?.original_title}`} src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}/>}
-        {/* <p>{movie?.overview}</p> */}
+        <p>{movie?.overview}</p>
         <p>{movie?.genre_ids?.map(genre_id => <p>{genres?.find(genre => genre.id === genre_id)?.name}</p>)}</p> 
-        {/* <Link to = {`/movie/${movie.id}`} state={{movie}}> */}
-            <button>Click Here for More Info</button>
-            {/* </Link> */}
-       
     </div>
 } 
